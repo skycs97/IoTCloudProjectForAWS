699f63450dd68e13659a99a2003ee0b56aa3c771
임철순 <skycs97@naver.com>
2019-12-14 17:49:13 +0900

    디바이스 현재 상태 조회 Lambda

---
+++ DeviceStateLambda/src/main/java/com/amazonaws/lambda/api/GetDeviceStateHandler.java
@@ -0,0 +1,27 @@
+package com.amazonaws.lambda.api;
+
+import com.amazonaws.services.iotdata.AWSIotData;
+import com.amazonaws.services.iotdata.AWSIotDataClientBuilder;
+import com.amazonaws.services.iotdata.model.GetThingShadowRequest;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class GetDeviceStateHandler implements RequestHandler<Event, String> {
+
+    @Override
+    public String handleRequest(Event event, Context context) {
+        AWSIotData iotData = AWSIotDataClientBuilder.standard().build();
+
+        GetThingShadowRequest getThingShadowRequest  = 
+        new GetThingShadowRequest()
+            .withThingName(event.device);
+
+        iotData.getThingShadow(getThingShadowRequest);
+
+        return new String(iotData.getThingShadow(getThingShadowRequest).getPayload().array());
+    }
+}
+
+class Event {
+    public String device;
+}
